b3c7f31b05ce2685b71784ad0809ac2dbdfa7746
ijishnupr <ijishnupr@gmail.com>
2024-11-17 22:38:37 +0530

    added audio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Main from './container/main';
-
 import Detatils from './container/details';
+import backgroundAudio from './container/audio.mpeg'; // Replace with the path to your audio file
 
 function App() {
   return (
     <div className="container">
-   
-      <div className="row">
+      {/* Audio */}
+      <audio autoPlay loop>
+        <source src={backgroundAudio} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
 
-        <div style={{height:"100vh"}} className="col-12 col-md-8 mx-auto">
+      {/* Main Content */}
+      <div className="row">
+        <div style={{ height: "100vh" }} className="col-12 col-md-8 mx-auto">
           <Main />
         </div>
       </div>
-      <div className='row'>
-        <div className='col'>
-
+      <div className="row">
+        <div className="col">
           <Detatils />
         </div>
       </div>
